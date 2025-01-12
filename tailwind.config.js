3ca8907707ad449cdd5198dfb416b86f6ccca6ec
Tayo <teeydigba@gmail.com>
2025-01-12 14:35:45 +0100

    added tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      "2xl": { min: "1600px" },
+      xl: { max: "1599px" },
+      ml: { max: "1450px" },
+      lg: { max: "1280px" },
+      md: { max: "1024px" },
+      sm: { max: "768px" },
+      "2xs": { max: "640px" },
+      xs: { max: "450px" },
+      tab: { min: "769px", max: "900px" },
+    },
   },
   plugins: [],
 };
