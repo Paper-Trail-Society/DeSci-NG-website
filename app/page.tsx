cac34c4a4a13b808597f228824b7051931910431
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2025-09-01 14:15:34 +0100

    Cleanup homepage layout and responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
-import { Text } from "./components/ui/text";
-import { Button } from "./components/ui/button";
-import { Input } from "./components/ui/input";
+import { Text } from "@/components/ui/text";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "./components/ui/card";
+} from "../components/ui/card";
 import { SearchIcon } from "lucide-react";
 
 // add 4 categories with name, tagline, and image URL. Categories - Applied science, arts & humanities, blockchain technology, medical science
@@ -39,29 +39,28 @@ const categories = [
 export default function Home() {
   return (
     <div className="font-sans items-center justify-items-center min-h-screen ">
+      <nav className="w-full 2xl:p-container-md pt-10 sm:pt-4">
+        <ul className="w-full md:w-[70%] px-4 md:px-2 md:mx-auto flex justify-between items-center">
+          <li>
+            <a href="#">
+              <Image
+                src="/assets/desci-ng-logo.png"
+                alt="logo"
+                width={100}
+                height={100}
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <Button variant={"destructive"} className="px-4">
+                LOGIN
+              </Button>
+            </a>
+          </li>
+        </ul>
+      </nav>
       <main className="flex flex-col items-center pb-20 w-full">
-        <nav className="w-full 2xl:p-container-xl pt-10 sm:pt-4">
-          <ul className="w-4/5 mx-auto flex justify-between items-center">
-            <li>
-              <a href="#">
-                <Image
-                  src="/assets/desci-ng-logo.png"
-                  alt="logo"
-                  width={100}
-                  height={100}
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <Button variant={"destructive"} className="px-4">
-                  LOGIN
-                </Button>
-              </a>
-            </li>
-          </ul>
-        </nav>
-
         <section className="md:w-1/3 w-full mx-auto my-10 space-y-6 px-8">
           <Text weight={"bold"} className="text-center leading-6 text-3xl">
             EXPLORE ACADEMIC PAPERS ACROSS DISCIPLINES
@@ -71,19 +70,19 @@ export default function Home() {
             <Input
               type="text"
               placeholder="Search papers by topic, author, or affiliated institution"
-              className="p-6 bg-[#F3E7E780] ring-1 ring-neutral-400 border-[#F3E7E780]/50 focus:border-[#F3E7E780]/50"
+              className="md:p-6 p-2 bg-[#F3E7E780] ring-1 ring-neutral-400 border-[#F3E7E780]/50 focus:border-[#F3E7E780]/50 placeholder:text-xs"
             />
 
-            <SearchIcon className="absolute w-4 h-4 top-4.25 right-3 text-[#0B0B0B]" />
+            <SearchIcon className="absolute w-3 h-3 top-3.25 right-1 md:top-4.25 md:right-3 text-[#0B0B0B]" />
           </div>
         </section>
         <section className="w-full">
-          <div className="flex flex-wrap gap-4 items-center justify-center">
+          <div className="flex flex-wrap gap-4 items-center justify-center mx-auto">
             {categories.map((category) => {
               return (
                 <Card
                   key={crypto.randomUUID()}
-                  className="justify-center border-none shadow-none flex flex-col gap-2 md:w-1/5 w-[18em]"
+                  className="justify-center border-none shadow-none flex flex-col gap-2 md:w-1/6 w-[18em]"
                 >
                   <CardHeader className="h-1/3 w-fit">
                     <Image
@@ -118,8 +117,9 @@ export default function Home() {
           </div>
         </section>
       </main>
-      <footer className="2xl:p-container-xl py-18 w-full border border-t-[var(--primary)]">
-        <div className="w-4/5 mx-auto space-y-4">
+
+      <footer className="w-full 2xl:p-container-md pt-10 sm:pt-4 border-t border-t-[var(--primary)]">
+        <div className="w-[70%] mx-auto space-y-4 md:mx-auto">
           <Image
             src="/assets/desci-ng-logo.png"
             alt="logo"
@@ -127,7 +127,7 @@ export default function Home() {
             height={100}
           />
 
-          <div className="flex flex-wrap items-center justify-between">
+          <div className="w-full px-4 md:px-2 md:mx-auto md:flex md:flex-wrap md:items-center md:justify-between space-y-4">
             <section>
               <ul className="space-y-2">
                 <li>
@@ -168,7 +168,7 @@ export default function Home() {
               </ul>
             </section>
 
-            <section className="flex flex-col justify-end pt-14">
+            <section className="flex flex-col justify-end md:pt-14">
               <Text size="sm"> &copy; {new Date().getFullYear()} Desci NG</Text>
             </section>
           </div>
