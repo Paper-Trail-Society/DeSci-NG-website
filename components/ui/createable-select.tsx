bf1a6b245b057ea7a036a5cc492688faa3c93a3a
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2025-09-02 23:08:47 +0100

    Add createable select component

---
+++ components/ui/createable-select.tsx
@@ -0,0 +1,156 @@
+import Select, { ActionMeta, MultiValueGenericProps, components, type CSSObjectWithLabel } from 'react-select';
+
+import Image, { StaticImageData } from 'next/image';
+
+import { cn } from '@/lib/utils/css';
+
+import CreatableSelect from 'react-select/creatable';
+
+export type SelectValueBase = { value: string; label: string; img?: string | StaticImageData };
+export type SelectValue = Omit<SelectValueBase, 'img'>;
+
+export type SelectProps = {
+  name: string;
+  placeholder: string;
+  options: SelectValueBase[];
+  handleChange: (arg: readonly SelectValue[], meta?: ActionMeta<SelectValueBase>) => void;
+  handleBlur?: () => void;
+  isSearchable?: boolean;
+  isCreatable?: boolean;
+  className?: string;
+  value?: readonly SelectValue[];
+  controlStyles?: CSSObjectWithLabel;
+  menuStyles?: CSSObjectWithLabel;
+  menuListStyles?: CSSObjectWithLabel;
+  optionStyles?: CSSObjectWithLabel;
+};
+
+const MultiValueContainer = (props: MultiValueGenericProps<SelectValueBase>) => {
+  return (
+    <div className="w-auto">
+      <components.MultiValueContainer {...props} />
+    </div>
+  );
+};
+
+const MultiSelect = ({
+  name,
+  options,
+  value,
+  placeholder,
+  handleChange,
+  handleBlur,
+  isSearchable = true,
+  isCreatable = false,
+  ...props
+}: SelectProps) => {
+  const Comp = isCreatable ? CreatableSelect : Select;
+  return (
+    <Comp
+      className={cn('text-capitalize px-0', props.className)}
+      components={{ MultiValueContainer }}
+      name={name}
+      options={options}
+      styles={{
+        input: base => ({
+          ...base,
+          color: '#000',
+        }),
+        control: (base, state) => ({
+          ...base,
+          borderRadius: '8px',
+          borderWidth: '2px',
+          border: 'none',
+          boxShadow: 'none',
+          fontSize: 12,
+          background: '#fff',
+          '&:hover': {
+            borderColor: '#045C5D',
+          },
+          ...props.controlStyles,
+        }),
+        menu: base => ({
+          ...base,
+          background: '#fff',
+          boxShadow: 'none',
+          border: '1px solid color-mix(in oklab, var(--primary) 90%, transparent);',
+          borderRadius: '0.1rem',
+          ...props.menuStyles,
+        }),
+        menuList: base => ({
+          ...base,
+          fontSize: '14px',
+          borderRadius: '0.1rem',
+          background: '#fff',
+          borderTopRightRadius: 0,
+          borderTopLeftRadius: 0,
+          padding: 0,
+          ...props.menuListStyles,
+        }),
+        option: (base, state) => ({
+          ...base,
+          color: '#000',
+          fontSize: '12px',
+          background: state.isFocused ? '#f5f5f5' : '',
+          '&:active': {
+            backgroundColor: '#f5f5f5',
+          },
+          ...props.optionStyles,
+        }),
+        multiValue: styles => ({
+          ...styles,
+          background: '#fff',
+          color: '#000',
+          border: '1px solid #dedcdc',
+          borderRadius: '5px',
+          justifyContent: 'space-between',
+          padding: '4px 6px',
+        }),
+        multiValueLabel: styles => ({
+          ...styles,
+          color: '#000',
+        }),
+        multiValueRemove: styles => ({
+          ...styles,
+          color: '#525252',
+          '&:hover': {
+            backgroundColor: '#dedcdc',
+            color: 'gray',
+          },
+        }),
+        valueContainer: styles => ({
+          ...styles,
+          padding: 0,
+        }),
+        indicatorSeparator: styles => ({
+          ...styles,
+          display: 'none',
+        }),
+        dropdownIndicator: styles => ({
+          ...styles,
+          color: '#8f8b8b',
+          '&:hover': {
+            color: '#000',
+          },
+        }),
+      }}
+      value={value}
+      isSearchable={isSearchable}
+      placeholder={placeholder}
+      onChange={(opt, meta) => {
+        handleChange(opt, meta);
+      }}
+      onBlur={handleBlur}
+      formatOptionLabel={option => (
+        <div className="flex items-center gap-2">
+          {option.img && <Image src={option.img} alt={'option-img'} style={{ width: '20px', borderRadius: '4px' }} />}
+          <span>{option.label}</span>
+        </div>
+      )}
+      isMulti
+    />
+  );
+};
+
+export { MultiSelect };
+
