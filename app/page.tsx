797c7f006d2bc5889e2ed0fe6385c23563d22b58
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2025-09-11 10:24:44 +0100

    Link paper categories to search page

--- app/page.tsx
+++ app/page.tsx
@@ -12,26 +12,28 @@ import {
   CardTitle,
 } from "../components/ui/card";
 
-// add 4 categories with name, tagline, and image URL. Categories - Applied science, arts & humanities, blockchain technology, medical science
-
 const categories = [
   {
     name: "Applied Science",
     tagline: "Discover papers in Biology, Chemistry, and more",
+    query: "science",
     imageUrl: "/assets/applied-science.png",
   },
   {
     name: "Arts & Humanities",
+    query: "arts",
     tagline: "Discover papers in Literature, Performing Arts, and more",
     imageUrl: "/assets/art-and-humanities.png",
   },
   {
     name: "Blockchain Technology",
+    query: "blockchain",
     tagline: "Discover papers in Distributed Ledger, Crypto, and more",
     imageUrl: "/assets/blockchain-tech.png",
   },
   {
     name: "Medical Science",
+    query: "medicine",
     tagline: "Discover papers in Cardiology, Anatomy, and more",
     imageUrl: "/assets/medical-science.png",
   },
@@ -62,27 +64,29 @@ export default function Home() {
                     key={crypto.randomUUID()}
                     className="justify-center border-none shadow-none flex flex-col gap-2 md:w-1/6 w-[18em]"
                   >
-                    <CardHeader className="h-1/3 w-fit">
-                      <Image
-                        src={category.imageUrl}
-                        alt={category.name}
-                        width={250}
-                        height={205}
-                        className="object-cover"
-                      />
-                    </CardHeader>
-                    <CardContent className="space-y-1">
-                      <CardTitle>{category.name.toUpperCase()}</CardTitle>
-                      <CardDescription>
-                        <Text
-                          size={"xs"}
-                          variant={"secondary"}
-                          className="text-wrap"
-                        >
-                          {category.tagline}
-                        </Text>
-                      </CardDescription>
-                    </CardContent>
+                    <Link href={`/search?query=${category.query}`}>
+                      <CardHeader className="h-1/3 w-fit">
+                        <Image
+                          src={category.imageUrl}
+                          alt={category.name}
+                          width={250}
+                          height={205}
+                          className="object-cover"
+                        />
+                      </CardHeader>
+                      <CardContent className="space-y-1">
+                        <CardTitle>{category.name.toUpperCase()}</CardTitle>
+                        <CardDescription>
+                          <Text
+                            size={"xs"}
+                            variant={"secondary"}
+                            className="text-wrap"
+                          >
+                            {category.tagline}
+                          </Text>
+                        </CardDescription>
+                      </CardContent>
+                    </Link>
                   </Card>
                 );
               })}
