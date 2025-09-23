1ecce0f1af2b9a91b977c028e1649d7d2b850e52
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2025-09-23 22:52:57 +0100

    Add homepage redirect to desci ng logo in footer

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -6,29 +6,37 @@ const Footer = () => {
   return (
     <footer className="w-full 2xl:p-container-md py-10 sm:pt-4 border-t border-t-[var(--primary)]">
       <div className="w-[70%] mx-auto space-y-4 md:mx-auto">
-        <Image
-          src="/assets/desci-ng-logo.png"
-          alt="logo"
-          width={100}
-          height={100}
-        />
+        <Link href="/">
+          <Image
+            src="/assets/desci-ng-logo.png"
+            alt="logo"
+            width={100}
+            height={100}
+          />
+        </Link>
 
         <div className="w-full px-4 md:px-2 md:mx-auto md:flex md:flex-wrap md:items-center md:justify-between space-y-4">
           <section>
             <ul className="space-y-2">
               <li>
                 <Link href="/about-us">
-                  <Text size={"sm"} weight={'medium'}>About Us</Text>
+                  <Text size={"sm"} weight={"medium"}>
+                    About Us
+                  </Text>
                 </Link>
               </li>
               <li>
                 <Link href="/how-to-upload-a-paper">
-                  <Text size={"sm"} weight={'medium'}>How to Upload a Paper</Text>
+                  <Text size={"sm"} weight={"medium"}>
+                    How to Upload a Paper
+                  </Text>
                 </Link>
               </li>
               <li>
                 <a href="mailto:info.descing@gmail.com">
-                  <Text size="sm" weight={'medium'}>Contact Us</Text>
+                  <Text size="sm" weight={"medium"}>
+                    Contact Us
+                  </Text>
                 </a>
               </li>
             </ul>
@@ -38,34 +46,47 @@ const Footer = () => {
             <ul className="space-y-2">
               <li>
                 <a href="https://x.com/DeSci_NG">
-                  <Text size="sm" weight={'medium'}>X (Formerly Twitter)</Text>
+                  <Text size="sm" weight={"medium"}>
+                    X (Formerly Twitter)
+                  </Text>
                 </a>
               </li>
               <li>
                 <Link href="https://www.linkedin.com/company/desci-ng">
-                  <Text size="sm" weight={'medium'}>LinkedIn</Text>
+                  <Text size="sm" weight={"medium"}>
+                    LinkedIn
+                  </Text>
                 </Link>
               </li>
               <li>
                 <Link href="https://www.instagram.com/desci__ng">
-                  <Text size="sm" weight={'medium'}>Instagram</Text>
+                  <Text size="sm" weight={"medium"}>
+                    Instagram
+                  </Text>
                 </Link>
               </li>
               <li>
                 <a href="http://tiktok.com/@desci_ng">
-                  <Text size="sm" weight={'medium'}>TikTok</Text>
+                  <Text size="sm" weight={"medium"}>
+                    TikTok
+                  </Text>
                 </a>
               </li>
-               <li>
+              <li>
                 <a href="https://www.youtube.com/@DeSci_NG">
-                  <Text size="sm" weight={'medium'}>YouTube</Text>
+                  <Text size="sm" weight={"medium"}>
+                    YouTube
+                  </Text>
                 </a>
               </li>
             </ul>
           </section>
 
           <section className="flex flex-col justify-end md:pt-14">
-            <Text size="sm" weight={'medium'}> &copy; {new Date().getFullYear()} Desci NG/Paper Trail Society.</Text>
+            <Text size="sm" weight={"medium"}>
+              {" "}
+              &copy; {new Date().getFullYear()} Desci NG/Paper Trail Society.
+            </Text>
           </section>
         </div>
       </div>
