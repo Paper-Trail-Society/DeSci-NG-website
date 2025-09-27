57daff60269810ca50cc43aa59dd7b34cdb43bc5
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2025-09-27 11:32:18 +0100

    updated about

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -18,8 +18,10 @@ const Page = () => {
           </Text>
 
           <div className="md:w-2/3 w-full text-left md:text-center flex flex-col gap-2 md:gap-4 px-4">
+                      <Text className="text-base md:text-lg">
+             We are Africa’s first decentralized research infrastructure, making African research open, secure, and globally accessible, forever.</Text>
             <Text className="text-base md:text-lg">
-             We are Africa’s first decentralized research infrastructure, making African research open, secure, and globally accessible, forever. For far too long, African scholarship remains siloed in institutions across the continent, limiting shared knowledge and the speed of innovation. We are reimagining this challenge by providing a collective where anyone can publish, share, and build on contextual African and related studies. 
+          For far too long, African scholarship remains siloed in institutions across the continent, limiting shared knowledge and the speed of innovation. We are reimagining this challenge by providing a collective where anyone can publish, share, and build on contextual African and related studies. 
             </Text>
 
             <Text className="text-base md:text-lg">
@@ -27,8 +29,11 @@ Today, our collective spans across West Africa and India, with the highest numbe
             </Text>
 
             <Text className="text-base md:text-lg">
-Leveraging blockchain technology, our infrastructure is purpose-built against loss, tampering, and being kept out of reach for the very people who need it most. This creates a multiplier effect for scholars within and outside the continent to consult contextual African studies, innovate to build custom solutions for Africa, and join forces with the rest of the world to address concerns that cannot wait. Our vision is an Africa where our discoveries endure and move the world forward.{" "}
+Leveraging blockchain technology, our infrastructure is purpose-built against loss, tampering, and being kept out of reach for the very people who need it most. This creates a multiplier effect for scholars within and outside the continent to consult contextual African studies, innovate to build custom solutions for Africa, and join forces with the rest of the world to address concerns that cannot wait.{" "}
             </Text>
+
+                        <Text className="text-base md:text-lg">
+            Our vision is an Africa where our discoveries endure and move the world forward.</Text>
           </div>
         </section>
       </div>
