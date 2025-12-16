fd5c5afa1189d56bbfd01afa314ca98b9cf30dfb
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2025-12-16 15:35:20 +0100

    Add see more/see less toggle to paper card

--- domains/paper/components/paper-card.tsx
+++ domains/paper/components/paper-card.tsx
@@ -1,13 +1,15 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Text } from "@/components/ui/text";
-import React from "react";
+import React, { useState } from "react";
 import { Paper } from "../types";
 import Link from "next/link";
 import { ABSTRACT_PREVIEW_LENGTH } from "../constants";
-
+import { Button } from "@/components/ui/button";
 
 const PaperCard = (props: Paper) => {
   const { title, abstract, user, id, slug, ipfsCid } = props;
+  const [isAbstractExpanded, setIsAbstractExpanded] = useState(false);
+
   return (
     <Card className="border-none">
       <CardHeader>
@@ -26,7 +28,25 @@ const PaperCard = (props: Paper) => {
         </Text>
         <Text size={"sm"}>
           <b>Abstract</b>:{" "}
-          {abstract.length > ABSTRACT_PREVIEW_LENGTH ? `${abstract.slice(0, ABSTRACT_PREVIEW_LENGTH)}...` : abstract}
+          <Text
+            size={"sm"}
+            className="leading-6 duration-300 transition-all ease-in-out"
+          >
+            {isAbstractExpanded
+              ? abstract
+              : abstract.length > ABSTRACT_PREVIEW_LENGTH
+              ? `${abstract.slice(0, ABSTRACT_PREVIEW_LENGTH)}...`
+              : abstract}
+          </Text>{" "}
+          {abstract.length > ABSTRACT_PREVIEW_LENGTH && (
+            <Button
+              variant={"primary"}
+              onClick={() => setIsAbstractExpanded(!isAbstractExpanded)}
+              className="hover:underline text-sm w-fit mx-auto text-text-link py-0 cursor-pointer"
+            >
+              {isAbstractExpanded ? "See less" : "See more"}
+            </Button>
+          )}
         </Text>
       </CardContent>
     </Card>
